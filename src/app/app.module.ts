fba1f303be6e61dea4adfec0adaad2bb3b3f5f6c
Sebas <seplarui@gmail.com>
2020-07-18 00:38:08 +0200

    css home-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
+import {MatGridListModule} from '@angular/material/grid-list';
 import { HomeComponent } from './components/home/home.component';
 import { GetTvOnTheAirComponent } from './get-tv-on-the-air/get-tv-on-the-air.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -20,7 +21,8 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSliderModule,
-    HttpClientModule
+    HttpClientModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
