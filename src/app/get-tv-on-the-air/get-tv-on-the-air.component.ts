b406142d12f7f7432c32aa03dfbbea1849556933
Sebas <seplarui@gmail.com>
2020-07-19 02:28:44 +0200

    json

--- src/app/get-tv-on-the-air/get-tv-on-the-air.component.ts
+++ src/app/get-tv-on-the-air/get-tv-on-the-air.component.ts
@@ -9,7 +9,7 @@ import { DataApiService } from '../services/data-api.service';
 })
 export class GetTvOnTheAirComponent implements OnInit {
 
-  tvShows: any[] = [];
+  tvShows: any[];
   constructor(private dataApi: DataApiService) { }
 
   ngOnInit(): void {
@@ -17,11 +17,28 @@ export class GetTvOnTheAirComponent implements OnInit {
   }
 
   getTvOnTheAir() {
+    // this.dataApi.getTvOnTheAir();
+    // this.dataApi.getTvOnTheAir().subscribe((data) => { this.tvShows = data['results']; },
+    //   (error) => { console.error(error); });
+    // this.dataApi.getTvOnTheAir().subscribe((response) => {
+    //   this.tvShows = response['results'];
+    // },
 
-    this.dataApi.getTvOnTheAir().subscribe((data) => { this.tvShows = data['results']; },
-      (error) => { console.error(error); });
+    //   (error) => console.log('KK ERROR: ' + error));
+    // console.log('kk ' + this.dataApi.getTvOnTheAir().subscribe((response) => {
+    //   this.tvShows = response['results'];
+    // }));
 
-    this.dataApi.getTvOnTheAir().subscribe((tvShows) => console.log(tvShows));
+    // this.dataApi.getTvOnTheAir().subscribe((tvShows) => console.log(tvShows));
+    this.dataApi.getTvOnTheAir().subscribe((response) => {
+      this.tvShows = response;
+      console.log("mierda " + this.tvShows['results'][0].name);
+      console.log("mierda 2 " + this.tvShows['page']);
+      console.log("mierda 3 " + this.tvShows['total_pages']);
+    });
+    console.log(this.tvShows);
+
+    // console.log("kk: " + this.tvShows);
   }
 
 }
--- src/app/services/data-api.service.ts
+++ src/app/services/data-api.service.ts
@@ -7,12 +7,24 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class DataApiService {
 
   constructor(private http: HttpClient) { }
-
+  apiKey = 'd1c58472015c64a45032584cd1373753';
 
   getTvOnTheAir() {
     const headers = new HttpHeaders().set('Content-Type', 'text/plain; charset=utf-8');
-    const urlApi = 'https://api.themoviedb.org/3/tv/on_the_air?api_key=d1c58472015c64a45032584cd1373753&language=es-ES&page=1';
-    return this.http.get(urlApi);
+
+    const urlApi = `https://api.themoviedb.org/3/tv/on_the_air?api_key=d1c58472015c64a45032584cd1373753&language=es-ES&page=1`;
+    console.log(urlApi);
+    console.log('DataApi: ' + this.http.get(urlApi));
+    return this.http.get<any>(urlApi);
+
+
+
+  }
+
+  // Detalles de una serie
+  getTVShow() {
+
+    const urlApi = ' https://api.themoviedb.org/3/tv/1403?api_key=d1c58472015c64a45032584cd1373753&language=es-ES';
   }
 }
 
